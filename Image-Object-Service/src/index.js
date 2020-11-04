a9b19dc147ef9124a910a77f6a1c7263f2a8afc1
Tiago Fragoso <e12005836@student.tuwien.ac.at>
2020-11-04 16:19:00 +0100

    [IOS #2] Add ESLint linter dependency and config

--- Image-Object-Service/src/index.js
+++ Image-Object-Service/src/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-	res.send("I'm working!")
-})
+app.get("/", (req, res) => {
+    res.send("I'm working!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at- http://localhost:${port}`);
-})
+    console.log(`Example app listening at- http://localhost:${port}`);
+});
