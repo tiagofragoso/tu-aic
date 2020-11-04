2ef2128d937c9ebef8d2df372f3933b1bbcc12fb
Tiago Fragoso <e12005836@student.tuwien.ac.at>
2020-11-04 16:48:24 +0100

    [IOS #2] Add MinIO dependency and setup basic connection

--- Image-Object-Service/src/index.js
+++ Image-Object-Service/src/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const minio = require("minio");
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => {
-    res.send("I'm working!");
+const minoClient = new minio.Client({
+    endPoint: "minio",
+    port: 9000,
+    useSSL: false,
+    accessKey: "minio",
+    secretKey: "minio123",
+});
+
+app.get("/", async (req, res) => {
+    try {
+        const buckets = await minoClient.listBuckets();
+        res.send(JSON.stringify(buckets));
+    } catch (err) {
+        console.error(err);
+        res.send("Error fetching buckets. Check the console");
+    }
 });
 
 app.listen(port, () => {
