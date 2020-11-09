9f111f4335f885054465ab24d88023fce43a3e8d
Tiago Fragoso <e12005836@student.tuwien.ac.at>
2020-11-09 22:44:19 +0100

    [IOS #2] Add morgan HTTP request logger

--- Image-Object-Service/src/loaders/index.js
+++ Image-Object-Service/src/loaders/index.js
@@ -1,7 +1,9 @@
 const loadApi = require("./api");
 const loadMinIO = require("./minio");
+const loadLogger = require("./logger");
 
 module.exports = async (app) => {
+    loadLogger(app); // always load logger before API routes
     loadApi(app);
     await loadMinIO();
     return app;
---
+++ Image-Object-Service/src/loaders/logger.js
@@ -0,0 +1,6 @@
+const morgan = require("morgan");
+
+module.exports = (app) => {
+    app.use(morgan("combined"));
+    return app;
+};
