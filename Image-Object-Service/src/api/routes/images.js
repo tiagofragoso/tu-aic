c070f40cc0a9290e1afacafd34d129e53fc6154c
Tiago Fragoso <e12005836@student.tuwien.ac.at>
2020-11-11 21:55:12 +0100

    [IOS #2] Add express-validator to validate HTTP requests

--- Image-Object-Service/src/api/routes/images.js
+++ Image-Object-Service/src/api/routes/images.js
@@ -1,52 +1,47 @@
 const { Router } = require("express");
 const StorageService = require("../../services/storage");
+const { get: get_validator, store: store_validator } = require("../middlewares/validators");
 
 const { FileNotFoundError, StorageServiceInternalError } = StorageService.errors;
 
 const router = Router();
 
-router.get("/:name", async (req, res, _next) => {
-    // Validate request (maybe move this to a middleware later)
-    const { name } = req.params;
-    if (!name) {
-        res.status(400).send();
-        return;
-    }
+// this validator is a fallback as it is rarely called because:
+// GET /images/ will not triger /images/:name route
+router.get("/:name", get_validator,
+    async (req, res, _next) => {
+        const { name } = req.params;
 
-    try {
+        try {
         // Retrieve image data and metadata from MinIO
-        const response = await StorageService.getImage(name);
-        res.send(response);
-    } catch (err) {
-        if (err instanceof FileNotFoundError) {
-            res.status(404).send("The requested file does not exist");
-        } else if (err instanceof StorageServiceInternalError) {
-            res.status(500).send("An error occurred while retrieving the image");
-        } else {
-            res.status(500).send("An error occurred");
+            const response = await StorageService.getImage(name);
+            res.send(response);
+        } catch (err) {
+            if (err instanceof FileNotFoundError) {
+                res.status(404).send("The requested file does not exist");
+            } else if (err instanceof StorageServiceInternalError) {
+                res.status(500).send("An error occurred while retrieving the image");
+            } else {
+                res.status(500).send("An error occurred");
+            }
         }
-    }
-});
+    });
 
-router.put("/", async (req, res, _next) => {
-    // Validate request (maybe move this to a middleware later)
-    const { name, imageFile } = req.body;
-    if (!name || !imageFile) {
-        res.status(400).send();
-        return;
-    }
+router.put("/", store_validator,
+    async (req, res, _next) => {
+        const { name, imageFile } = req.body;
 
-    try {
+        try {
         // Store the base64 image
-        await StorageService.storeImageBase64(name, imageFile);
-        res.status(200).send();
-    } catch (err) {
-        if (err instanceof StorageServiceInternalError) {
-            res.status(500).send("An error occurred while storing the image");
-        } else {
-            res.status(500).send("An error occurred");
+            await StorageService.storeImageBase64(name, imageFile);
+            res.status(200).send();
+        } catch (err) {
+            if (err instanceof StorageServiceInternalError) {
+                res.status(500).send("An error occurred while storing the image");
+            } else {
+                res.status(500).send("An error occurred");
+            }
         }
-    }
-});
+    });
 
 module.exports = router;
