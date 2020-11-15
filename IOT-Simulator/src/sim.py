a8be8ecfad5ee3ba2cdf2873b09538ec6171fcbf
Nils Moynac <e12016308@student.tuwien.ac.at>
2020-11-15 12:39:48 +0100

    changing the location of the data repo in the sim.py file

--- IOT-Simulator/src/sim.py
+++ IOT-Simulator/src/sim.py
@@ -9,7 +9,7 @@ def getdata_encoded(my_dic) :
     rdm_nbr = int(random.random()*len(my_dic))
     pic_name = my_dic[rdm_nbr]["filename"] # find a random picture through the repo
     # print(pic_name)
-    file_name = "./iwildcam_synthesized_idaho/images/"+pic_name
+    file_name = "../data/iwildcam_synthesized_idaho/images/"+pic_name
     with open(file_name, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()) # encode the picture
     # print(encoded_string)
@@ -26,29 +26,9 @@ def post(url_test, data_test = None):
     print("--- BODY --- \n",post_test.text)
 
 
-
-
-if __name__ == '__main__':
-    data_repo = "./iwildcam_synthesized_idaho/"
-    if len(sys.argv) > 2 :
-        print("Too many arguments")
-        print("You should only put the directory of the pictures to send")
-        print("e.g : pythhon3 sim.py ./iwildcam_synthesized_idaho/")
-        sys.exit(0)
-    elif len(sys.argv) == 2 :
-        with open(sys.argv[1]+"metadata.json") as f:
-            pictures_dic = json.load(f)
-        data = getdata_encoded(pictures_dic)
-        post("http://localhost:3000/users/data", data)
-    else :
-        with open("./iwildcam_synthesized_idaho/metadata.json") as f:
-            pictures_dic = json.load(f)
-        data = getdata_encoded(pictures_dic)
-        post("http://localhost:3000/users/data", data)
-else :
-    with open("./iwildcam_synthesized_idaho/metadata.json") as f:
-        pictures_dic = json.load(f)
-    data = getdata_encoded(pictures_dic)
-    post("http://localhost:3000/users/data", data)
+with open("../data/iwildcam_synthesized_idaho/metadata.json") as f:
+    pictures_dic = json.load(f)
+data = getdata_encoded(pictures_dic)
+post("http://localhost:3000/users/data", data)
 
 # Help with mockoon post : https://mockoon.com/docs/latest/templating/
