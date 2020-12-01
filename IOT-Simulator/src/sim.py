dae520caa2440666b1712e362e3814b1652615d7
Tiago Fragoso <e12005836@student.tuwien.ac.at>
2020-12-01 21:43:15 +0100

    Fix IoT simulator communication with middleware

--- IOT-Simulator/src/sim.py
+++ IOT-Simulator/src/sim.py
@@ -17,25 +17,26 @@ API_ENDPOINT = os.getenv("API_ENDPOINT")
 
 # Encode the data to send it to the port
 def getdata_encoded(my_dic) :
-    rdm_nbr = int(random.random()*len(my_dic))
+    rdm_nbr = int(random.random() * len(my_dic))
     pic_name = my_dic[rdm_nbr]["filename"] # find a random picture through the repo
-    file_name = PATH+"/images/"+pic_name
+    file_name = PATH + "/images/" + pic_name
     with open(file_name, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()) # encode the picture
-    data_encoded = {"image" : encoded_string, "metadata" : json.dumps(my_dic[rdm_nbr])} # add the metadata to send a json
+    data_encoded = {"image" : str(encoded_string), "metadata" : my_dic[rdm_nbr]} # add the metadata to send a json
+    # print(data_encoded)
     return data_encoded
 
-# Send the data to the port
-def post(url_test, data_test = None):
+# POST the data to the middleware
+def post(url, data):
     try :
-        post_test = requests.post(data=data_test, url=url_test)
-    except :
-        raise Exception("No post possible for the following url : ",url_test)
-    status = post_test.status_code
-    if status != 200:
-        raise Exception("Wrong status code : ", status)
-    print("--- HEADERS --- \n", post_test.headers)
-    print("--- BODY --- \n",post_test.text)
+        post_test = requests.post(url, json=data)
+    except requests.exceptions.RequestException as e:
+        print("Error sending image", e)
+        return
+
+    status_code = post_test.status_code
+    if status_code != requests.codes.created:
+        print("Error sending image. Got HTTP", status_code)
 
 # Load the metadata
 with open(PATH+"/metadata.json") as f:
@@ -48,9 +49,9 @@ if __name__ == '__main__':
 
 # Script to send the pictures to the PORT
 for i in range(47) : # 47 is the number of data that I put on little_data
-    rdm_wait = int(random.random()*20) # a picture is sent every <20 s
+    rdm_wait = int(random.random() * 20) # a picture is sent every <20 s
     time.sleep(rdm_wait)
     data = getdata_encoded(pictures_dic)
-    post(str(API_ENDPOINT)+"/events", data)
+    post(str(API_ENDPOINT) + "/events", data)
 
 # Help with mockoon post : https://mockoon.com/docs/latest/templating/
