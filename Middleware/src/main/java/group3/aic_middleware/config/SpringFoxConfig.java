a5e0a9ea651d2ae5fb071fe722fa4bcedd07af4a
Dominik Fenzl <e1526544@student.tuwien.ac.at>
2020-12-30 16:07:02 +0100

    added swagger support
    Url can be found under http://127.0.0.1:9999/swagger-ui/index.html

---
+++ Middleware/src/main/java/group3/aic_middleware/config/SpringFoxConfig.java
@@ -0,0 +1,48 @@
+package group3.aic_middleware.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(
+                PathSelectors.any()).build().pathMapping("/").apiInfo(new ApiInfoBuilder()
+                .title("Federated Storage Middleware")
+                .description("Federated Storage for IOT Sensing Events")
+                .build());
+    }
+
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//
+//        // Allow anyone and anything access. Probably ok for Swagger spec
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//
+////        source.registerCorsConfiguration("/api/event-history/current/traffic-lights", config);
+////        source.registerCorsConfiguration("/api/event-history/current/cars", config);
+////        source.registerCorsConfiguration("/api/event-history/*", config);
+//
+////        source.registerCorsConfiguration("/api/**", config);
+////        source.registerCorsConfiguration("*/**", config);
+////        source.registerCorsConfiguration("/**", config);
+////        source.registerCorsConfiguration("/v2/api-docs", config);
+//        return new CorsFilter(source);
+//    }
+}
