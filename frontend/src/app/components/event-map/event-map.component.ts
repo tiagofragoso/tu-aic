eff2d6229ebc30657aadf9a318c98d20c9a70e9d
Tiago Fragoso <e12005836@student.tuwien.ac.at>
2021-01-01 19:07:00 +0000

    Frontend: Add popup content to events map

--- frontend/src/app/components/event-map/event-map.component.ts
+++ frontend/src/app/components/event-map/event-map.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 
 import {latLng, tileLayer, icon, marker, Layer, LatLng, Map, circle, point} from 'leaflet';
 
 import {EventService} from 'src/app/services/event.service';
 import {EventTableRow} from '../../models/event-table-data';
+import {convertUnixDateToString} from "../../utils/date";
 
 @Component({
   selector: 'app-event-map',
@@ -12,6 +13,15 @@ import {EventTableRow} from '../../models/event-table-data';
   styleUrls: ['./event-map.component.css']
 })
 export class EventMapComponent {
+
+  // Popup content is rendered as static HTML so we can't bind (click), see used fix below:
+  // https://github.com/Asymmetrik/ngx-leaflet/issues/60#issuecomment-493716598
+  @HostListener('document:click', ['$event']) 
+    clickout(event: any) {
+      if (event.target.classList.contains("popup-link")){ 
+        this.eventClicked(event.target.dataset.eventId); 
+      } 
+    }
 
   baseLayer = tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 14, minZoom: 1 });
   center: LatLng = latLng(42.854912879552664, -115.06896547973157);
@@ -85,8 +95,64 @@ export class EventMapComponent {
         shadowUrl: 'leaflet/marker-shadow.png'
       })  
     });
-    m.bindPopup(`<span (click)="eventClicked(${event.event_id})">${event.event_id}</span>`);
+    m.bindPopup(this.createPopupHtml(event), {minWidth: 200});
     return m;
+  }
+
+  private createPopupHtml(event: EventTableRow) {
+    return `<div>
+      <div class="row">
+        <div class="col-4 text-muted">
+          Name:
+        </div>
+        <div class="col-8 fw-bold">
+          ${event.name}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 text-muted">
+          State:
+        </div>
+        <div class="col-8">
+          ${event.state}
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col-4 text-muted">
+          Place ID:
+        </div>
+        <div class="col-8">
+          ${event.place_ident}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 text-muted">
+          Created: 
+        </div>
+        <div class="col-8">
+          ${convertUnixDateToString(event.created)}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 text-muted">
+          Updated: 
+        </div>
+        <div class="col-8">
+          ${convertUnixDateToString(event.updated)}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 text-muted">
+          Tags: 
+        </div>
+        <div class="col-8">
+          ${event.tags.length > 1 ? event.tags.length - 1 : 'Not tagged yet'}
+        </div>
+      </div>
+      <div class="row mt-2 justify-content-center">
+        <button class="btn btn-link btn-sm popup-link" data-event-id="${event.event_id}">See event details</button>
+      </div>
+    </div>`;
   }
 
   public eventClicked(id: string) {
