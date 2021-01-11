2deef56bb22cc4ece36ea28b0a6dd1fa8f38299e
Nils Moynac <e12016308@student.tuwien.ac.at>
2021-01-11 20:48:29 +0100

    more images CRUDed

--- Workflow/src/workflow.py
+++ Workflow/src/workflow.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 from iotSimulator import postEvent
 from aiSimulator import addTag
 from dataManager import getOrderedData, DOCKER
@@ -19,29 +21,33 @@ categoriesToChange = ["name", "device_id", "longitude", "latitude", "datetime", 
 # This is done to mock an iot device that sends pictures according to the time they are taken
 picturesDic = getOrderedData()
 
-
-# -- CREATE 10 IMAGES
+# -- CREATE IMAGES
 print(" ------ IOT SIMULATOR ------")
-for i in range(10) :
+for i in range(40) :
     postEvent(picturesDic[i])
 
-# -- CHANGE METADATA TO 2 IMAGES
+# -- CHANGE METADATA TO IMAGES
 print(" ------ METADATA MODIFIER ------")
 # Either precise the new attribute
 changeMetadata(picturesDic[0],"name","my_device_new_name")
 
 # Either not
-changeMetadata(picturesDic[1],"longitude")
+for i in range(10) :
+    rdmIndex = random.randint(0,len(categoriesToChange)-1)
+    changeMetadata(picturesDic[2*i+1],categoriesToChange[rdmIndex])
 
-# ADD TAGS TO 3 IMAGES
+# ADD TAGS TO IMAGES
 print(" ------ AI SIMULATOR ------")
-addTag(picturesDic[1])
-addTag(picturesDic[5])
-addTag(picturesDic[9])
+for i in range(10) :
+    rdm = random.randint(0,5)
+    for j in range(rdm) :
+        addTag(picturesDic[2*i])
 
 # DELETING 1 IMAGE
 print(" ------ IMAGE ERASER ------")
-delete(picturesDic[3]["seq_id"])
+delete(picturesDic[1]["seq_id"])
+
+# TODO : make an image faulty (put to the image-object service)
 
 
 
