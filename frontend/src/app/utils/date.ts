92e801f935a3fc230a7ab47a82570e8a5ab89225
Kevin <kevin.al-chater@upstream-mobility.at>
2021-01-18 18:17:45 +0100

    Changed date methods to local time

--- frontend/src/app/utils/date.ts
+++ frontend/src/app/utils/date.ts
@@ -1,17 +1,12 @@
 import * as moment from "moment";
 
 export const convertUnixDateToString = (date: Date) => {
-  return moment.utc(date).format('DD.MM.YYYY - HH:mm');
+  return moment(date.valueOf()).format('DD.MM.YYYY - HH:mm');
 };
 
 export const convertUnixDateToDate = (d: number) => {
-  const date = new Date(d);
-  const utcDate = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-    date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
-  return new Date(utcDate);
-};
-
-export const convertStringDateToDate = (d: string) => {
-  // example string: '13-Apr-2019 (04:35:23.000000)'
-  return new Date(moment.utc(d, 'DD.MMM.YYYY').toDate());
+  // const date = new Date(d);
+  //const utcDate = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
+  // date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+  return new Date(d);
 };
